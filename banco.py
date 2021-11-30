d30e35d424c0e9673ba3adadb9899490f8330424
viniciussilvax <viniciusdasilvax@hotmail.com>
2021-11-30 15:44:20 -0300

    Arquivo inicial banco.py

---
+++ banco.py
@@ -0,0 +1,44 @@
+from typing import List
+from time import sleep
+
+from models.cliente import Cliente
+from models.conta import Conta
+
+
+contas: List[Conta] = []
+
+
+def main() -> None:
+    menu()
+
+
+def menu() -> None:
+    pass
+
+
+def criar_conta() -> None:
+    pass
+
+
+def sacar() -> None:
+    pass
+
+
+def depositar() -> None:
+    pass
+
+
+def transferir() -> None:
+    pass
+
+
+def listar_contas() -> None:
+    pass
+
+
+def buscar_conta_numero(numero: int) -> Conta:
+    pass
+
+
+if __name__ == '__main__':
+    main()
