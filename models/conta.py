5ec1da7a02e16b89292a1eb9287f704c7e8a68f6
viniciussilvax <viniciusdasilvax@hotmail.com>
2021-11-30 15:20:23 -0300

    Models Conta

---
+++ models/conta.py
@@ -0,0 +1,41 @@
+from models.cliente import Cliente
+from utils.helper import formato_moeda
+
+
+class Conta:
+    id: int = 10
+
+    def __init__(self: object, cliente: Cliente) -> None:
+        self.__numero: int = Conta.id
+        self.__cliente: Cliente = cliente
+        self.__saldo: float = 0.0
+        self.__limite: float = 100
+        self.__saldo__total: float = self._calcula_saldo_total
+
+    @property
+    def numero(self: object) -> int:
+        return self.__numero
+
+    @property
+    def cliente(self: object) -> Cliente:
+        return self.__cliente
+
+    @property
+    def saldo(self: object) -> float:
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self: object, valor: float) -> None:
+        self.__saldo = valor
+
+    @property
+    def limite(self: object) -> float:
+        return self.__limite
+
+    @limite.setter
+    def limite(self: object, valor: float) -> float:
+        self.__limite = valor
+
+    @property
+    def _calcula_saldo_total(self: object) -> float:
+        return self.saldo + self.limite
